ddfd082f11610b5e5b8d0ab33b8b207eb7f21906
anandnic <ananddey.nic@gmail.com>
2021-08-05 00:37:43 +0530

    nav to be done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,9 @@
         </nav>
 
         <main class="py-4">
+            <div class="float-left">
+                @include('layouts.sidebar')
+            </div>
             @yield('content')
         </main>
     </div>
